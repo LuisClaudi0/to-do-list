9dd14a4f453566fda0d13f15ec16fcbe6ede43fb
Luis Claudio <luisclaudiobar@gmail.com>
2025-01-16 21:22:21 -0300

    enviar projeto

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>TO DO LIST</h1>
+
+<h2>📃📃 About</h2>
+<p>Project for to do list, with edit, check and delete tasks by developer Luis Claudio.</p>
+
+## 🚀🚀 Technologies
+<div>
+  <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</div>
---
+++ app.js
@@ -0,0 +1,64 @@
+let tarefas = [];
+
+let button = document.getElementById('button');
+let input = document.getElementById('userInput');
+
+
+button.addEventListener('click', function(){
+    sendTask();
+    input.value = '';
+});
+
+input.addEventListener('keydown', function(){
+    if (event.key == "Enter") { 
+        sendTask();
+        input.value = '';
+    }
+});
+
+function sendTask(){
+    if(input.value != ''){
+
+        let div = document.createElement('div');
+        let task = document.getElementById('userInput').value;
+        let showTask = document.createElement('p');
+        showTask.textContent = task;
+        showTask.classList.add('task-format');
+
+        let edit = document.createElement('button');
+        edit.classList.add('button', 'edit');
+        edit.addEventListener('click', function(){
+            let newInput = document.createElement('input');
+            newInput.classList.add('task-format');
+            newInput.classList.add('new-input');
+            showTask.remove();
+            check.remove();
+            div.prepend(newInput);
+            div.prepend(check);
+        });
+
+        let delet = document.createElement('button');
+        delet.classList.add('button', 'delete');
+        delet.addEventListener('click', function(){
+            div.remove();
+        });
+
+        let check = document.createElement('button');
+        check.classList.add('button', 'done', 'invisible'); 
+        check.addEventListener('click', function(){
+            check.classList.toggle('invisible');
+            showTask.classList.toggle('line-through')
+        });
+
+        div.prepend(check);
+        div.appendChild(showTask);
+        div.appendChild(edit);
+        div.appendChild(delet);
+        div.classList.add('div-ajuste');
+        document.getElementById('tasks__camp').appendChild(div);
+        
+
+    } else {
+        alert('Por favor, diga qual tarefa deseja adicionar!');
+    }
+};
